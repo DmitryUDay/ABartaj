08f5ec7ada82c57bb03ad601a44983f23bd4d192
Дмитрий <147247493+DmitryUDay@users.noreply.github.com>
2025-09-09 19:53:55 +0300

    Add files via upload

--- server/js/imager.js
+++ server/js/imager.js
@@ -1,9 +1,8 @@
 function goBack() {
     if (document.referrer) {
-        // Если есть предыдущая страница в истории
         window.history.back();
     } else {
-        // Если нет истории — перенаправляем на главную
-        window.location.href = "index.html";
+        // Перейти на index.html с полной перезагрузкой
+        window.location.replace("index.html");
     }
 }
